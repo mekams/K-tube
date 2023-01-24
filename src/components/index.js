616b9a42f73747022f8f7aab11df6b3d9e8888ce
Kamran <kamraneg@gmail.com>
2023-01-25 02:58:26 +0530

    sidebar complete

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
-//exported  in single line  to App.js
-export { default as Navbar } from './Navbar';
+//exported  in single line  to App.js                                  // sequence  of creation
+export { default as Navbar } from './Navbar';                   //main----1
 export { default as ChannelDetail } from './ChannelDetail';
-export { default as Feed } from './Feed';
+export { default as Feed } from './Feed';                       //sub-----2
 export { default as SearchFeed } from './SearchFeed';
 export { default as VideoDetail } from './VideoDetail';
-export { default as SearchBar} from './SearchBar';
-export { default as SideBar} from './SideBar';
+export { default as SearchBar} from './SearchBar';           //split------3
+export { default as SideBar} from './SideBar';              //split------4
 
 
