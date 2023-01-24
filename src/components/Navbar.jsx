efc86c1c476f76fcedf647b69ce08a3121184034
Kamran <kamraneg@gmail.com>
2023-01-25 02:20:13 +0530

    sidebar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import Searchbar from "./SearchBar";                  //importing component on which this interface code dependent
 
 import logo from "../exported_utils/youtube.png";   //logo
+import { Margin } from "@mui/icons-material";
 
 const Navbar = () => {
   return (
@@ -21,7 +22,7 @@ const Navbar = () => {
     >
       {/* imported logo  is set to '/' */}
       <Link to="/" style={{ display: "flex", aligntems: "center" }}>
-        <img src={logo} alt="logo" height={45}></img>
+        <img src={logo} alt="logo" height={45}></img>  {/*<h1 style={{color:"white",margin:"-5px 0px 0px 10px", fontSize:"5rem"}}>K-tube</h1>*/}
       </Link>
 
       {/* SearchBar components is split */}
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import { Stack } from "@mui/material";
+import { categories } from "../exported_utils/constants"; //imported categories for youtube sidebar
+
+const selectedCategory = "New"; //can also define same in betwwen line18 and 19
 
 const SideBar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    // Flex of sidebar is created , direction is row by default and changes in medium devices to column
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      {/* using map function importing the name & icons object stored in constants.js file(exported_Utils) */}
+      {categories.map((category) => {
+        return (
+          <button
+            className="category-btn"
+            style={{
+              background: category.name === selectedCategory && "#Fc1503",
+              color: "white",
+            }}
+            //Each property requires a key whenever using map in react----------- contd...further
+            key={category.name}
+          >
+            <span
+              style={{
+                color: category.name === selectedCategory ? "white" : "red",
+                marginRight: "15px",
+              }}
+            >
+              {category.icon}
+            </span>
+            <span
+              style={{
+                opacity: category.name === selectedCategory ? "1" : "0.8",
+              }}
+            >
+              {category.name}
+            </span>
+          </button>
+        );
+      })}
+    </Stack>
+  );
+};
 
-export default SideBar
+export default SideBar;
