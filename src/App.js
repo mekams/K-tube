25a705e6cf0d25a923a42fb943d6649d09211877
Kamran <kamraneg@gmail.com>
2023-01-24 16:37:39 +0530

    route created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import { BrowserRouter , } from 'react-router-dom'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';  // {browser router as Router} can also be used
+import { Box } from '@mui/system';
+
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       
-    </div>
+      {/*Fetching layout box from material ui with prop sx */}
+      <Box sx={{backgroundColor: '#000'}}>  
+        Navbar
+      </Box>
+
+      <Routes>
+        {/* created route to visit to specific components */}
+        <Route path = '/' exact element={<Feed/>}/>
+        <Route path = '/video/:id' element={<VideoDetail/>}/>
+        <Route path = '/channel/:id' element={<ChannelDetail/>}/>
+        <Route path = '/search/:serachTerm' element={<Search/>}/>
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
