0cc9a54ef5ae1a73d67e919c991cd29053573a77
Kamran <kamraneg@gmail.com>
2023-01-28 04:39:58 +0530

    readme added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Stack, Box, responsiveFontSizes } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import logo from "../utility/youtube.png";
@@ -21,7 +21,18 @@ const Navbar = () => (
     {/* imported logo  is set to '/' */}
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />{" "}
-       <h1 style={{color:"white",margin:"-5px 0px 0px 10px", fontSize:"5rem"}}>K-tube</h1>
+      {/* <h1 style={{color:"white",margin:"-5px 0px 0px 10px", fontSize:"5rem"}}>K-tube</h1> */}
+      <Box
+        sx={{
+          color: "#FfFF",
+          border: "none",
+          fontSize: "2.5rem",
+          fontFamily:"cursive",
+          margin: "0px 1px 0px 10px",
+        }}
+      >                
+        K-tube    
+      </Box>
     </Link>
 
     {/* SearchBar components is split */}
